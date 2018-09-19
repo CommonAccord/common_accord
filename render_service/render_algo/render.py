01cdc98a9aeccd338c099568363adbfc9e3eeb29
Xiaoman Chu <chuxiaoman@xiaomans-air.devices.brown.edu>
2018-09-19 16:36:16 -0400

    Skeleton of Graph and Rendering algorithm

---
+++ render_service/render_algo/render.py
@@ -0,0 +1,12 @@
+"""
+Core Render Algorithm. We can either have this file, or just wrap all of these in graph.py
+"""
+
+def render(graph, key):
+    pass
+
+def find(grpah, key):
+    pass
+
+def search(graph, depref):
+    pass
