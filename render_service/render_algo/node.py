52cbb23e0c366040bf896e3f84b2104659ad63e4
Xiaoman Chu <chuxiaoman@xiaomans-air.devices.brown.edu>
2018-10-03 14:43:52 -0400

    graph -> node

--- render_service/render_algo/node.py
+++ render_service/render_algo/node.py
@@ -2,26 +2,31 @@
 Core render algorithm wrapped in the class Graph
 """
 import json
-import RenderTree from render_tree
+# import RenderTree from render_tree
 
-class Graph(object):
+class Node(object):
     """
-    A Graph is a dictionary with a predefined order
+    Node is the data structure that represents a legal document.
+    Its most notable fields are:
+
+    edges: a list of references to other documents with string associations
+    data: a dictionary of local key-List<Token> pairs
+    name: name of document (also used for dfs)
     """
     # NOTE: This implementation requires python 3.6 or above because it assumes
     #       that dictionaries maintain the ordering of keys.
 
-    def __init__(self, refs, nonrefs, id):
+    def __init__(self, refs, nonrefs, name):
         """
         Not yet implemented
         Assuming that there's a list
         """
-        # dict: key -> Graph
-        self.refs = refs
+        # list of string
+        self.edges = refs
         # dict: key -> List<Tokens>
-        self.nonrefs = nonrefs
-        # id used for dfs
-        self.id = id
+        self.data = nonrefs
+        # name used for dfs
+        self.name = name
 
 
     def render(self, key):
@@ -30,22 +35,25 @@ class Graph(object):
 
         Returns the result of rendering the input key
         """
-        # TODO: In Progress
-        s = []
-        acc = RenderTree()
-        # NOTE: Do we error handle for invalid key?
-        s[0:0] = find(self, key) #adding stack returned by find onto s stack
-
-        while s:
-            t = s.pop(0)
-            if type(t) is Literal:
-                acc.add_leaf(t)
-            elif type(t) is Variable:
-                acc.add_subtree(t)
-                # add the new list of tokens to the top of the stack
-                s[0:0] = find(self, key)
-
-        return acc
+        root = {}
+        unfinished = [(key, root)]
+        while unfinished:
+            var, tree = unfinished.pop()
+            tokens, metadata = self.find(var)
+            tree["metadata"] = metadata
+            parts = tree.setdefault("parts", [])
+            accumulated = []
+            for token in tokens:
+                if isinstance(token, int): # Literal
+                    parts.append(token)
+                elif isinstance(token, str): # Variable
+                    parts.append(dict())
+                    accumulated.insert(0, (token, {}))
+                else: # If correctly implemented, this should never happen
+                    raise ValueError
+            unfinished += accumulated
+        # When everything is done, return the root
+        return root
 
 
     def find(self, key):
@@ -64,18 +72,20 @@ class Graph(object):
         # len key is the length of the prefixes + self
         for i in range(len(key)):
             depref = key[i:]
-            v = search(self, sum(depref))
+            v = self.search("".join(depref))
             if v is not None:
                 return v
             else:
                 continue
         return None # this should be a custom error message
 
+
+
     def search(self, target):
         g = self
         stack = []
         visited = []
-.       stack.append({"prefixes" : [], "path" : [self.id], "node": g})
+.       stack.append({"prefixes" : [], "path" : [self.name], "node": g})
         # can code in regex later... I'm not convinced of performance bump
         while stack:
             s = stack.pop()
@@ -84,19 +94,18 @@ class Graph(object):
             else:
                 visited.append(s)
                 # add prefixes to target to create final value to match
-                prefixed = sum(s["prefixes"]) + target
-                if prefixed is in self.nonrefs.keys():
+                prefixed = "".join(s["prefixes"]) + target
+                if prefixed is in self.data:
                     # we found what we're looking for
-                    return {"path": s["path"], 
-                        "value": self.nonrefs[item]}
+                    return {"path": s["path"], "value": self.data[item]}
                 # we need to continue searching
                 # add keys in reverse so that the first one gets popped first
-                for k in self.refs.keys()[::-1]:
+                for k in self.edges.keys()[::-1]:
                     child_to_push = {"prefixes": s["prefixes"].append(k),
-                        "path": s["path"].append(self.refs[k].id),
-                        "node": self.refs[k]}
+                        "path": s["path"].append(self.edges[k].name),
+                        "node": self.edges[k]}
                     stack.append(child_to_push)
-        
+
         # This should be an error message ("Item not found")
         return None
 
