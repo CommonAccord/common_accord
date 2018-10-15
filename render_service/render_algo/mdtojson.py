38160574de2c3dd6342720f0c13d926a7856d436
Geoffrey Hazard <Geoffrey_Hazard@brown.edu>
2018-10-15 16:50:13 -0400

    Wrote getMap funct in mdtojson.py
    getMap() method converts a specific map from .md to json given the .md filename.

---
+++ render_service/render_algo/mdtojson.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import json
+
+def getMap(map):
+    """Takes in a string that is the name of an .md file 
+    and returns a json string
+    Takes in name of file without the .md extension
+    """
+
+    h = { "data":{}, "edges":{} }
+    for line in open(map+".md"):
+        line = line[0:line.find("\n")] # removing newline char
+        if "=" in line and not line.endswith("="):
+            keyval = line.split("=")
+            k = keyval[0]
+            v = keyval[1]
+
+            if "[" and "]" in v: # add to edges
+                v = v[v.find("[")+1:v.find("]")] # removing brackets
+                h["edges"][k]=v
+            else: #add to data
+                if not k in h:
+                    h["data"][k]=v
+
+    return json.dumps(h)
+
+if __name__ == '__main__' :
+    print "testing getMap..."
+    print getMap("test/Easy_Test_maps/Alice")
+    print "testing done"
