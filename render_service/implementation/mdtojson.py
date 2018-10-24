dd59e9743032c68498cd311f8b889450749e26f0
Xiaoman Chu <chuxiaoman@xiaomans-air.devices.brown.edu>
2018-10-24 16:20:06 -0400

    terminal ui

--- render_service/implementation/mdtojson.py
+++ render_service/implementation/mdtojson.py
@@ -2,6 +2,34 @@
 import json
 import re
 
+def m2j(name, directory):
+    """
+    md2json with fewer function calls, which means better performance ??
+    """
+    graph = dict()
+    jdict = {"root": name, "graph": graph}
+    unfinished = [name]
+    while unfinished:
+        current = unfinished.pop()
+        if current not in graph:
+            graph[current] = {"data": {}, "edges": []}
+            with open(directory + "/" + current + ".md") as f:
+                for line in f:
+                    line = line.rstrip("\n")
+                    for i, c in enumerate(line):
+                        if "=" == c:
+                            k = line[:i]
+                            v = line[i+1:]
+                            if v and v[0] == "[" and v[-1] == "]":
+                                graph[current]["edges"].append([k, v[1:-1]])
+                            else:
+                                graph[current]["data"].setdefault(k, re.compile("[\\{\\}]").split(v))
+                            break
+            for edge, neighbor in graph[current]["edges"]:
+                unfinished.append(neighbor)
+    return json.dumps(jdict)
+
+
 def md2json(name, directory):
     """
     Takes a name of a md file (without postfix) and a directory and produce a
