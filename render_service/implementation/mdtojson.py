4b01729318dbc8ba554640e46420e9ef7ab5385a
jamesghazard <james.g.hazard@gmail.com>
2018-10-24 19:30:04 -0700

    Remove the ".md" implied file extension.

--- render_service/implementation/mdtojson.py
+++ render_service/implementation/mdtojson.py
@@ -13,7 +13,7 @@ def m2j(name, directory):
         current = unfinished.pop()
         if current not in graph:
             graph[current] = {"data": {}, "edges": []}
-            with open(directory + "/" + current + ".md") as f:
+            with open(directory + "/" + current) as f:
                 for line in f:
                     line = line.rstrip("\n")
                     for i, c in enumerate(line):
@@ -41,7 +41,7 @@ def md2json(name, directory):
     while unfinished:
         current = unfinished.pop()
         if current not in graph:
-            graph[current] = getMap(directory + "/" + current + ".md")
+            graph[current] = getMap(directory + "/" + current)
             for edge, neighbor in graph[current]["edges"]:
                 unfinished.append(neighbor)
     return json.dumps(jdict)
