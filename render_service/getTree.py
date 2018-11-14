59ff5fe5a35715a09cd63f7e13785ba91dd95ae4
Geoffrey Hazard <Geoffrey_Hazard@brown.edu>
2018-11-14 15:19:47 -0500

    pretty prints in getTree.py

--- render_service/getTree.py
+++ render_service/getTree.py
@@ -2,6 +2,8 @@ import sys
 from implementation.mdtojson import md2json
 from render_algo.node import Node
 
+import pprint
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
@@ -12,4 +14,6 @@ if __name__ == "__main__":
     root_node = Node.parse(md2json(rname, directory))
 
     for key in keys:
-        print(root_node.render(key))
+
+        pp = pprint.PrettyPrinter(indent=4) #prints data structures nicely
+        pp.pprint(root_node.render(key))
