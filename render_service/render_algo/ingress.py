1562427692e242160045bf74ce8bc6d217afb864
Jerry Chu <chuxiaoman411@gmail.com>
2018-11-28 16:42:24 -0500

    ingress comments

--- render_service/render_algo/ingress.py
+++ render_service/render_algo/ingress.py
@@ -3,6 +3,16 @@ import json
 import sys
 
 def main(argv):
+    """
+    Takes in the first argument the key
+    the second argument is the json string
+
+    What's returned is the rendered tree with the following structure
+
+    Tree = {metadata: {path: [edge path to where found], names: [vertex path ... ]},
+            children: A list of Trees
+            text: The variable name that resides originally}
+    """
     root_node = Node.parse(" ".join(argv[1:]))
     tree = root_node.render(argv[0])
     print(json.dumps(tree))
